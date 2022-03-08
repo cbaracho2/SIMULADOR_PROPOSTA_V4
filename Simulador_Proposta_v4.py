78499a1241b291dc359682410fd0de6df22f3b28
cbaracho2 <94143215+cbaracho2@users.noreply.github.com>
2022-03-08 20:57:37 -0300

    Update Simulador_Proposta_v4.py

--- Simulador_Proposta_v4.py
+++ Simulador_Proposta_v4.py
@@ -572,7 +572,7 @@ else:
         BASE_CALCULO.fillna(value=0, inplace=True)
         st.dataframe(BASE_CALCULO.style.format(subset=["SINAL","MENSAIS","MENSAIS_2","MENSAIS_POS","SEMESTRAIS","ANUAIS", "FINANC","EV_OBRA","EV_OBRA_1","TOTAL"], formatter="{:.2f}"))
         
-        Gerar_data_imagem = dfi.export(RESULTADO, 'Solicitar_Aprovacao.png',chrome_path=None)
+        Gerar_data_imagem = dfi.export(RESULTADO, 'Solicitar_Aprovacao.png')
 
         add_image(Imagem_data,imagem_Logo,LISTA_EMPREENDIMENTOS,LISTA_BLOCOS,LISTA_UNIDADES,resposta_proposta())  
         
